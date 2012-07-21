ffd3eba28be6f3abc04bc6469184c07b9238f012
Samuel Le <samuel.le@gmail.com>
2012-07-21 16:45:23 +0100

    started quantification of qualitative vars

--- src/R/clusteringUsers.R
+++ src/R/clusteringUsers.R
@@ -10,4 +10,25 @@ questsCols <<- 9:27
 
 quantVars <<- c(3,9:27)
 
+if(!exists("workingVals")) {workingVals <<- names(summary(users[,4]))}
 
+#making "gender" variable quantitative
+quantGender <- function(gender) {
+  return(ifelse(gender=="Female",1,0))
+}
+
+#making "working" variable quantitative
+quantWorking <- function(working,valSet) {
+  qualVal <- which(workingVals==working)
+  res <- valSet[qualVal]
+  return(res)
+}
+
+getQuantWorking <- function(valSet) {
+  res <- function(x) {
+    return(quantWorking(x,valSet))
+  }
+  return(res)
+}
+
+qtWorking <<-getQuantWorking(c(0,40,20,8,25,25,15,0,30,0,5,5,15))
