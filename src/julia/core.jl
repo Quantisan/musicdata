44fbf16fc2de4471e57db8f8bf5c721703f21031
Paul Lam <paul.lam@forward.co.uk>
2012-07-21 13:37:38 +0100

    reading in files, index out of range error

---
+++ src/julia/core.jl
@@ -0,0 +1,3 @@
+users = csvread("/Users/paullam/Dropbox/Projects/musicdata/data/users.csv");  ## couldn't get getcwd() working
+train = csvread("/Users/paullam/Dropbox/Projects/musicdata/data/train.csv");
+words = csvread("/Users/paullam/Dropbox/Projects/musicdata/data/words.csv");  ## 20mb, index out of range error
