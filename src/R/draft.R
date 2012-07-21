9fd7585ade7a6625b520cdacf8b8586d225d1fb9
Paul Lam <paul.lam@forward.co.uk>
2012-07-21 15:56:00 +0100

    setwd src/R

--- src/R/draft.R
+++ src/R/draft.R
@@ -1,10 +1,10 @@
-#setwd("~/Rcode/hackathonMusic/musicdata/")
+#setwd("~/Rcode/hackathonMusic/musicdata/src/R")
 
-if(!exists("test")) {test <- read.csv("data/test.csv")} #125794 * 4
-if(!exists("train")) {train <- read.csv("data/train.csv")} #188690 * 5
-if(!exists("users")) {users <- read.csv("data/users.csv")} #48645 * 27
-if(!exists("words")) {words <- read.csv("data/words.csv")} #118301 * 88
-if(!exists("userKey")) {userKey <-  read.csv("data/UserKey.csv")}
+if(!exists("test")) {test <- read.csv("../../data/test.csv")} #125794 * 4
+if(!exists("train")) {train <- read.csv("../../data/train.csv")} #188690 * 5
+if(!exists("users")) {users <- read.csv("../../data/users.csv")} #48645 * 27
+if(!exists("words")) {words <- read.csv("../../data/words_proper.csv")} #118301 * 88
+if(!exists("userKey")) {userKey <-  read.csv("../../data/UserKey.csv")}
 #words: 50 artists, ~ 50k users
 
 #train: 50 artists, ~ 50k users
