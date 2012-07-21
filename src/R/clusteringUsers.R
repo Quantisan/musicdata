cc4fb1168a138172584fbef3bc2711e48eae41bf
Samuel Le <samuel.le@gmail.com>
2012-07-21 15:56:20 +0100

    added clustering suff (just at the beginning)

---
+++ src/R/clusteringUsers.R
@@ -0,0 +1,13 @@
+userCol <<- 1
+genderCol <<- 2
+ageCol <<- 3
+workingCol <<-4
+regionCol <<- 5
+musicCol <<- 6
+listOwnCol <<- 7
+listBackCol <<- 8
+questsCols <<- 9:27
+
+quantVars <<- c(3,9:27)
+
+
