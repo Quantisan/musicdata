347680b8cb93a28ceddbb11dcdfe50186f993479
Samuel Le <samuel.le@gmail.com>
2012-07-21 17:34:14 +0100

    started to cluster users

--- src/R/clusteringUsers.R
+++ src/R/clusteringUsers.R
@@ -12,6 +12,7 @@ quantVars <<- c(3,9:27)
 
 if(!exists("workingVals")) {workingVals <<- names(summary(users[,4]))}
 
+if(!exists("musicVals")) {musicVals <<- names(summary(users[,6]))}
 #making "gender" variable quantitative
 quantGender <- function(gender) {
   return(ifelse(gender=="Female",1,0))
@@ -31,4 +32,49 @@ getQuantWorking <- function(valSet) {
   return(res)
 }
 
+#randomly created function to make variable "working" quantitative
 qtWorking <<-getQuantWorking(c(0,40,20,8,25,25,15,0,30,0,5,5,15))
+
+quantMusic <- function(music, valSet) {
+  qualVal <- which(musicVals == music)
+  return(valSet[qualVal])
+}
+
+getQuantMusic <- function(valSet) {
+  res <- function(x) {
+    return(quantMusic(x,valSet))
+  }
+}
+
+#randomly created function to make variable "music" quantitative
+qtMusic <<- getQuantMusic(c(2,0,6,6,4,10))
+
+#need to make REGION quantitative as well. the problem is that regions are not ordered in any way...
+
+convertHours <- function(hour,naVal=0) {
+  if(hour=="Less than an hour") {
+    return(0)
+  }
+  if(hour == "More than 16 hours") {
+    return(16)
+  }
+  if(hour == "") {
+    return(naVal)
+  }
+  res <- strsplit(hour," ")[[1]][1]
+  return(res)
+}
+
+distUsers <- function(u1,u2,usersData,qData) {
+  if(u1 == u2) {
+    return(0)
+  }
+}
+
+calcDistMat <- function(usersData) {
+  quantData <- usersData[,quantVars]
+  quantData <- cbind(quantData,sapply(qtWorking,usersData[,4]))
+  quantData <- cbind(quantData,sapply(qtMusic,usersData[,6]))
+  quantData <- apply(quantData,2,function(x){x/sd(x)})
+  
+}
