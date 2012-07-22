b949ff3d5b420cee1a6fbc076a50e9213ad75b15
Samuel Le <samuel.le@gmail.com>
2012-07-22 10:52:53 +0100

    added preparation of data

---
+++ src/R/regWords.R
@@ -0,0 +1,10 @@
+wordsKeyed <- cbind(paste(words[,1],words[,2]),words)
+testKeyed <- cbind(paste(test[,1],test[,3]),test)
+trainKeyed <- cbind(paste(train[,1],train[,3]),train)
+
+names(wordsKeyeds)[1] <- "key"
+names(testKeyed)[1] <- "key"
+names(trainKeyed)[1] <- "key"
+
+dataReg <- merge(wordsKeyed,trainKeyed,by="key")
+dataToPredict <- merge(wordsKeyed,testKeyed,by="key")
--- src/R/regressionsWithWords.R
+++ src/R/regressionsWithWords.R
@@ -60,3 +60,43 @@ sds <<- sapply(artists,getSD)
 
 
 #merging test with words...
+
+nWords <- nrow(words)
+#mergedTrain <- NULL
+
+#words2 <- words[18283:nWords,]
+#nWords <- nrow(words2)
+mergedTest <- NULL
+start <- 1
+tmpSeq <- seq(start,nWords,1000)
+nSeqs <- length(tmpSeq) - 1
+for(j in 1:nSeqs) {
+  #print(i)
+  print(j)
+  startBucket <- tmpSeq[j]
+  endBucket <- tmpSeq[j+1]-1
+  mergedTest2 <- NULL
+  for(i in startBucket:endBucket) {
+    ## if(i%%1000 == 0) {
+    ##   print(i)
+    ## }
+    artist <- words[i,1]
+    user <- words[i,2]
+                                        #tmpFunc <- getUserAndArtist(user,artist)
+    tmpData <- train[which(test[,3]==user),]
+    if(nrow(tmpData) > 0 ) {
+      tmpData <- tmpData[which(tmpData[,1]==artist),]
+      
+      #rating <- tmpData[,4]#train[which(apply(train,1,tmpFunc)),4]
+      if(NROW(tmpData)>0) {
+                                        #print("now filling stuff...")
+        if(NROW(rating)>1) {
+          rating <- mean(rating)
+        }
+        subData <- c(artist,user,words2[i,])
+        mergedTest2 <- rbind(mergedTest2,subData)
+      }
+    }
+  }
+  mergedTest <- rbind(mergedTest,mergedTest2)
+}
